b25d1d1fab26b6b0f2c6d0f6d69c350e1d0b89aa
Law  Michael Dennis <lawmich3@ug57.eecg>
2014-03-17 19:07:17 -0400

    lab3 images

--- lab3/part3/matlabplot_p3.m
+++ lab3/part3/matlabplot_p3.m
@@ -5,10 +5,10 @@ clc;clear all;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 close all;
 clearvars;
-size = 5000;
+size = 4000;
 
 %............. sink output1
-[packet_no_p, packetsize_p, arrival_time] = textread('output_sink1.txt', '%f %f %f');
+[packet_no_p, packetsize_p, arrival_time] = textread('output_sink3.txt', '%f %f %f');
 
 %plot number of packet transmissions on a time scale of 10 ms per data
 %point
@@ -20,14 +20,14 @@ subplot(2,1,1);
 time = arrival_time(1:size);
 packet = packet_no_p (1:size);
 plot(cumsum(time), packet);
-title('Packet Tranmissions for N=8');
+title('Packet Tranmissions for N=2');
 xlabel('time (in microseconds)');
 ylabel('number of packets');
 %plot number of transmitted bytes on a time scale of 10 ms per data point
 subplot(2,1,2);
 packet2 = packetsize_p(1:size);
 plot(cumsum(time), cumsum(packet2));
-title('Number of Bytes Transmitted for N=8');
+title('Number of Bytes Transmitted for N=2');
 ylabel('number of bytes');
 xlabel('time (in microseconds)');
 

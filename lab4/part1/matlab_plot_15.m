03c84034cac3f9b355825e0636568c42a88a7e6d
Law  Michael Dennis <lawmich3@ug51.eecg>
2014-04-06 23:15:37 -0400

    lab4 part 1 done

--- lab4/part1/matlab_plot_15.m
+++ lab4/part1/matlab_plot_15.m
@@ -1,8 +1,8 @@
-[seqNo, send, receive] = textread('output_sink_15_1.txt', '%d %d %d');
+[seqNo, send, receive] = textread('output_sink_15_3.txt', '%d %d %d');
 
 figure(1);
 plot(seqNo,send,seqNo,receive);
-title('Packet train: N=100, L=400, r=10');
+title('Packet train: N=100, L=400, r=10000');
 xlabel('Sequence Numbers');
 ylabel('Time (in microseconds)');
 legend('Send','Receive');
